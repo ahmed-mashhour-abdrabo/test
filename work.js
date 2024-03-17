12835edb7db7e267a443e21feb74ee604d5f9c6a
ahmed mashhour <157679096+ahmed-mashhour-abdrabo@users.noreply.github.com>
2024-03-17 09:05:45 +0200

    Create work.js

---
+++ work.js
@@ -0,0 +1,25 @@
+console.log("ahmed mashhour");
+//_3ndVb fbgy3m38 ft2m32mm oq31bsqd nu34rnf1
+//tvf2evcx oq44ahr5 lb5m6g5c svlsagor p2rjqpw5 epia9gcq
+const wait = (seconds) => {
+    myVar = setTimeout(() => { }, seconds * 1000);
+    clearTimeout(myVar);
+}
+
+
+
+for (; true;) {
+    wait(3);
+    console.log("test ");
+    wait(3);
+    const collection = document.getElementsByClassName("tvf2evcx oq44ahr5 lb5m6g5c svlsagor p2rjqpw5 epia9gcq")[0];
+    wait(3);
+    if (collection) {
+        collection.click();
+    }
+}
+
+
+// setInterval(() => {
+ 
+// }, 3000);
